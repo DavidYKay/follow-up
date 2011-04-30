eddb629ba9815aa12f48a1397bf409b15cdfd267
David Young-Chan Kay <davidykay@gmail.com>
2011-04-30 15:58:34 -0400

    Added basic test skeletons.

---
+++ Follow UpTest/src/com/followupapp/reminders/test/AlarmTest.java
@@ -0,0 +1,24 @@
+package com.followupapp.reminders.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Test our basic alarm logic.
+ * 
+ * @author dk
+ *
+ */
+public class AlarmTest extends InstrumentationTestCase {
+
+	public void testCancelAlarm() {
+		// Create alarms
+		// Cancel them
+		
+		// Make sure none go off
+		
+	}
+	
+	public void testAlarm() {
+		
+	}
+}
---
+++ Follow UpTest/src/com/followupapp/reminders/test/ContentObservableTest.java
@@ -0,0 +1,16 @@
+package com.followupapp.reminders.test;
+
+import android.test.InstrumentationTestCase;
+
+public class ContentObservableTest extends InstrumentationTestCase {
+
+	/**
+	 * Test if we can properly monitor the SMS ContentProvider.
+	 */
+	public void testContentObservable() {
+		// Mock out the SMS ContentProvider
+		// Create a ContentObserver
+		// Modify the contentProvider
+		// ContentObserver should have fired
+	}
+}
