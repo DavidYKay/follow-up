7387761d2c239e78782b230715f21beccf0cb9ab
Heliodor Jalba <heliodorj@gmail.com>
2011-04-30 19:43:31 -0400

    added reply button. doesn't do anything.

--- Follow Up/src/com/followupapp/reminders/ReminderActivity.java
+++ Follow Up/src/com/followupapp/reminders/ReminderActivity.java
@@ -15,8 +15,11 @@ public class ReminderActivity extends Activity {
         
         String smsSource = getIntent().getStringExtra(SmsIncomingReceiver.SMS_SOURCE);
         
+        TextView replyDescription = (TextView)findViewById(R.id.replyDescription);
+        replyDescription.setText("It looks like you haven't replied to the text recently received from " + smsSource + ".");
+
         TextView ignoreDescription = (TextView)findViewById(R.id.ignoreDescription);
-        ignoreDescription.setText("If you don't want to get reminders for text messages received from " + smsSource + ", just ignore it.");
+        ignoreDescription.setText("If you don't want to get any more reminders for text messages received from " + smsSource + ", just ignore it.");
 
         Button ignoreButton = (Button)findViewById(R.id.ignoreButton);
         ignoreButton.setOnClickListener(new OnClickListener() {
