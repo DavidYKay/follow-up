1cb62f60ae828ef21a579e075e810dd502ae2421
David Young-Chan Kay <davidykay@gmail.com>
2011-04-30 15:46:48 -0400

    Added three skeleton activities.

--- Follow Up/src/com/followupapp/reminders/MainActivity.java
+++ Follow Up/src/com/followupapp/reminders/MainActivity.java
@@ -3,6 +3,12 @@ package com.followupapp.reminders;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * Main activity of the application. Presents a list of people that the user has not yet followed up with.   
+ * 
+ * @author dk
+ *
+ */
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
---
+++ Follow Up/src/com/followupapp/reminders/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.followupapp.reminders;
+
+import android.app.Activity;
+
+/**
+ * Activity for presenting the application settings to the user.
+ * 
+ * @author dk
+ *
+ */
+public class SettingsActivity extends Activity {
+
+}
