81c619ab5f516c03ae63d8a5ca0ca17aecee37e4
Heliodor Jalba <heliodorj@gmail.com>
2011-04-30 19:35:00 -0400

    added ignore button. doesn't do anything.

--- Follow Up/src/com/followupapp/reminders/ReminderActivity.java
+++ Follow Up/src/com/followupapp/reminders/ReminderActivity.java
@@ -2,18 +2,27 @@ package com.followupapp.reminders;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * Main activity of the application. Presents a list of people that the user has not yet followed up with.   
- * 
- * @author dk
- *
- */
 public class ReminderActivity extends Activity {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.reminder);
+        
+        String smsSource = getIntent().getStringExtra(SmsIncomingReceiver.SMS_SOURCE);
+        
+        TextView ignoreDescription = (TextView)findViewById(R.id.ignoreDescription);
+        ignoreDescription.setText("If you don't want to get reminders for text messages received from " + smsSource + ", just ignore it.");
+
+        Button ignoreButton = (Button)findViewById(R.id.ignoreButton);
+        ignoreButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			}
+        });
     }
 }
